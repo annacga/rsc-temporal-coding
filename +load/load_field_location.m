8ecff9d82e493e45a7c2fca867c04de20e6e0dee
annacga <166383866+annacga@users.noreply.github.com>
2025-05-16 15:45:40 +0200

    Update load_field_location.m

--- +load/load_field_location.m
+++ +load/load_field_location.m
@@ -6,8 +6,7 @@ else
     load_data = 1:length(data)-1;
 end
 
-loaddir = '/Users/annachristinagarvert/UIO Physiology Dropbox Dropbox/Lab Data/Malte Bieler/Analysis/areas/classification';
-loaddir = '/Users/annachristinagarvert/UIO Physiology Dropbox Dropbox/Lab Data/Malte Bieler/Fig_1/field_info_modes/';
+loaddir = '/Fig_1/field_info_modes/';
 
 for i = load_data
     for f = 1:length(data(load_data(i)).sessionIDs)
