972f48886d0368a09e53430582feec61587d9307
annacga <166383866+annacga@users.noreply.github.com>
2025-05-16 16:27:42 +0200

    Update field_info.m

--- +analysis/field_info.m
+++ +analysis/field_info.m
@@ -77,148 +77,143 @@ else
     load_data = 1:length(data)-1;
 end
 
-Fs = 31/5;
-% lowpass filtered (< 1Hz)
-Fpass = 1;
-delay = 1:44; whichSignal = 'deconv';
+% -------------------------------------------------------------------------
+% ANALYSIS PARAMETERS
+% -------------------------------------------------------------------------
+Fs = 31/5;       % effective sampling rate (after smoothing in rMaps)
+Fpass = 1;       % cutoff frequency for low-pass filter (<1Hz)
+delay = 1:44;    % time bins corresponding to trial delay period
+whichSignal = 'deconv';  % which signal to analyze (typically 'deconv')
 
+% Output directory for saving results
 savedir = '/Fig_1/field_info_modes';
 
 for i = load_data
     for f = 1:length(data(load_data(i)).sessionIDs)
-        
+
+        % Get cell indices per condition
         TimeA = mData(load_data(i),f).TimeA.indices;
         TimeB = mData(load_data(i),f).TimeB.indices;
         OdorA = mData(load_data(i),f).OdorA.indices;
         OdorB = mData(load_data(i),f).OdorB.indices;
-        
-        % field index, field size ,activation_probability,
-        % peak_time_variance for time A cells
-        
-        %         [fieldSize,field_idx,peak_time_variance,activation_probability,firing_field]
-        [fieldSize,field_idx, peak_time_variance,activation_probability,firing_field]...
-            = single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsAA,mData(load_data(i),f).(whichSignal).rmapsAB,TimeA);
-        
+
+        % ---- TIME A CELLS ----
+        [fieldSize, field_idx, peak_time_variance, activation_probability, firing_field] = ...
+            single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsAA, ...
+                                              mData(load_data(i),f).(whichSignal).rmapsAB, TimeA);
+
+        % Store results
         mData(load_data(i),f).TimeA.field_size             = fieldSize;
         mData(load_data(i),f).TimeA.field_location         = field_idx;
-        mData(load_data(i),f).TimeA.activation_probability = 100*activation_probability;
+        mData(load_data(i),f).TimeA.activation_probability = 100 * activation_probability;
         mData(load_data(i),f).TimeA.peak_time_variance     = peak_time_variance;
         mData(load_data(i),f).TimeA.firing_field           = firing_field;
-        
-        
-        if ~exist(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f}))
-            mkdir(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f}))
+
+        % Save to disk if requested
+        sessionPath = fullfile(savedir, data(load_data(i)).area, data(load_data(i)).sessionIDs{f});
+        if ~exist(sessionPath, 'dir')
+            mkdir(sessionPath)
         end
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeA_fieldSize'),'fieldSize')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeA_field_location'),'field_idx')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeA_activation_probability'),'activation_probability')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeA_peak_time_variance'),'peak_time_variance')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeA_firing_field'),'firing_field')
-        
-        
-        % odor A
-        [fieldSize,field_idx, peak_time_variance,activation_probability,firing_field]...
-            = single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsAA,mData(load_data(i),f).(whichSignal).rmapsAB,OdorA);
-        
+        save(fullfile(sessionPath, 'TimeA_fieldSize'), 'fieldSize');
+        save(fullfile(sessionPath, 'TimeA_field_location'), 'field_idx');
+        save(fullfile(sessionPath, 'TimeA_activation_probability'), 'activation_probability');
+        save(fullfile(sessionPath, 'TimeA_peak_time_variance'), 'peak_time_variance');
+        save(fullfile(sessionPath, 'TimeA_firing_field'), 'firing_field');
+
+        % ---- ODOR A CELLS ----
+        [fieldSize, field_idx, peak_time_variance, activation_probability, firing_field] = ...
+            single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsAA, ...
+                                              mData(load_data(i),f).(whichSignal).rmapsAB, OdorA);
+
         mData(load_data(i),f).OdorA.field_size             = fieldSize;
         mData(load_data(i),f).OdorA.field_location         = field_idx;
-        mData(load_data(i),f).OdorA.activation_probability = 100*activation_probability;
+        mData(load_data(i),f).OdorA.activation_probability = 100 * activation_probability;
         mData(load_data(i),f).OdorA.peak_time_variance     = peak_time_variance;
         mData(load_data(i),f).OdorA.firing_field           = firing_field;
-        size(field_idx)
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorA_fieldSize'),'fieldSize')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorA_field_location'),'field_idx')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorA_activation_probability'),'activation_probability')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorA_peak_time_variance'),'peak_time_variance')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorA_firing_field'),'firing_field')
-        
-        % field index, field size ,activation_probability,
-        % peak_time_variance for time B cells
-        [fieldSize,field_idx, peak_time_variance,activation_probability,firing_field]...
-            = single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsBA,mData(load_data(i),f).(whichSignal).rmapsBB,TimeB);
-        
+
+        save(fullfile(sessionPath, 'OdorA_fieldSize'), 'fieldSize');
+        save(fullfile(sessionPath, 'OdorA_field_location'), 'field_idx');
+        save(fullfile(sessionPath, 'OdorA_activation_probability'), 'activation_probability');
+        save(fullfile(sessionPath, 'OdorA_peak_time_variance'), 'peak_time_variance');
+        save(fullfile(sessionPath, 'OdorA_firing_field'), 'firing_field');
+
+        % ---- TIME B CELLS ----
+        [fieldSize, field_idx, peak_time_variance, activation_probability, firing_field] = ...
+            single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsBA, ...
+                                              mData(load_data(i),f).(whichSignal).rmapsBB, TimeB);
+
         mData(load_data(i),f).TimeB.field_size             = fieldSize;
         mData(load_data(i),f).TimeB.field_location         = field_idx;
-        mData(load_data(i),f).TimeB.activation_probability = 100*activation_probability;
+        mData(load_data(i),f).TimeB.activation_probability = 100 * activation_probability;
         mData(load_data(i),f).TimeB.peak_time_variance     = peak_time_variance;
         mData(load_data(i),f).TimeB.firing_field           = firing_field;
-        
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeB_fieldSize'),'fieldSize')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeB_field_location'),'field_idx')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeB_activation_probability'),'activation_probability')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeB_peak_time_variance'),'peak_time_variance')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'TimeB_firing_field'),'firing_field')
-        
-        
-        [fieldSize,field_idx, peak_time_variance,activation_probability,firing_field]...
-            = single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsBA,mData(load_data(i),f).(whichSignal).rmapsBB,OdorB);
-        
+
+        save(fullfile(sessionPath, 'TimeB_fieldSize'), 'fieldSize');
+        save(fullfile(sessionPath, 'TimeB_field_location'), 'field_idx');
+        save(fullfile(sessionPath, 'TimeB_activation_probability'), 'activation_probability');
+        save(fullfile(sessionPath, 'TimeB_peak_time_variance'), 'peak_time_variance');
+        save(fullfile(sessionPath, 'TimeB_firing_field'), 'firing_field');
+
+        % ---- ODOR B CELLS ----
+        [fieldSize, field_idx, peak_time_variance, activation_probability, firing_field] = ...
+            single_cell_field_characteristics(mData(load_data(i),f).(whichSignal).rmapsBA, ...
+                                              mData(load_data(i),f).(whichSignal).rmapsBB, OdorB);
+
         mData(load_data(i),f).OdorB.field_size             = fieldSize;
         mData(load_data(i),f).OdorB.field_location         = field_idx;
-        mData(load_data(i),f).OdorB.activation_probability = 100*activation_probability;
+        mData(load_data(i),f).OdorB.activation_probability = 100 * activation_probability;
         mData(load_data(i),f).OdorB.peak_time_variance     = peak_time_variance;
         mData(load_data(i),f).OdorB.firing_field           = firing_field;
-        
-        
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorB_fieldSize'),'fieldSize')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorB_field_location'),'field_idx')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorB_activation_probability'),'activation_probability')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorB_peak_time_variance'),'peak_time_variance')
-        save(fullfile(savedir,data(load_data(i)).area,data(load_data(i)).sessionIDs{f},'OdorB_firing_field'),'firing_field')
-        
-        
+
+        save(fullfile(sessionPath, 'OdorB_fieldSize'), 'fieldSize');
+        save(fullfile(sessionPath, 'OdorB_field_location'), 'field_idx');
+        save(fullfile(sessionPath, 'OdorB_activation_probability'), 'activation_probability');
+        save(fullfile(sessionPath, 'OdorB_peak_time_variance'), 'peak_time_variance');
+        save(fullfile(sessionPath, 'OdorB_firing_field'), 'firing_field');
     end
-    
-    
+end
+
 end
 
 
+function [fieldSize, field_idx, peak_time_variance, activation_probability, firing_field] = ...
+    single_cell_field_characteristics(rmaps1, rmaps2, cellIdx)
 
+    Fs = 31/5;        % sampling rate
+    Fpass = 1;        % low-pass filter cutoff
+    delay = 1:38;     % time bins to consider (effective trial window)
 
-function [fieldSize,field_idx,peak_time_variance,activation_probability,firing_field] = single_cell_field_characteristics(rmaps1,rmaps2,cellIdx)
-        
-        % original signal was sampled with 31Hz but in rmaps 3 time points are
-        % always averaged
-        Fs = 31/5;
-        % lowpass filtered (< 1Hz)
-        Fpass = 1;
-        delay = 1:38;
-        
-        %delay = 1:105;
-        
-        fieldSize              = NaN(length(cellIdx),1);
-        field_idx              = NaN(length(cellIdx),1);
-        activation_probability = NaN(length(cellIdx),1);
-        peak_time_variance     = NaN(length(cellIdx),1);
-        firing_field           = cell(length(cellIdx),1);
-        
-        for m = 1:length(cellIdx)
-            rmap                              = [rmaps1(:,delay,cellIdx(m));...
-                rmaps2(:,delay,cellIdx(m))];
-            mean_rmaps                        =  nanmean(rmap);
-            lowpass_mean_rmaps                = lowpass(mean_rmaps,Fpass,Fs);
-            
-            [field_idx(m),firing_field{m}]    = analysis.find_firing_field(lowpass_mean_rmaps);
-            fieldSize(m)                      = length(firing_field{m});
-            
-            % calcuate activation_probability
-            if ~isnan(field_idx(m))
-                activeTrials                     = find(sum([rmaps1(:,delay(field_idx(m)),cellIdx(m));...
-                    rmaps2(:,delay(field_idx(m)),cellIdx(m))],2)>0);
-                activation_probability(m)        = length(activeTrials)/size(rmap,1);
-                
-                % calcuate firing peak time variance
-                [~,field_idx_trial] = max(rmap');
-                
-                peak_time_variance(m)     = nanvar(field_idx_trial*0.1613);
-                
-            else
-                activation_probability(m) = NaN;
-                peak_time_variance(m)     = NaN;
-            end
-            
+    % Preallocate outputs
+    n = length(cellIdx);
+    fieldSize              = NaN(n,1);
+    field_idx              = NaN(n,1);
+    activation_probability = NaN(n,1);
+    peak_time_variance     = NaN(n,1);
+    firing_field           = cell(n,1);
+
+    % Loop over cells
+    for m = 1:n
+        % Combine across conditions (AA and AB or BA and BB)
+        rmap = [rmaps1(:, delay, cellIdx(m)); rmaps2(:, delay, cellIdx(m))];
+
+        % Average across trials and low-pass filter
+        mean_rmaps = nanmean(rmap);
+        smoothed = lowpass(mean_rmaps, Fpass, Fs);
+
+        % Find firing field and peak index
+        [field_idx(m), firing_field{m}] = analysis.find_firing_field(smoothed);
+        fieldSize(m) = length(firing_field{m});
+
+        % If a valid field was found
+        if ~isnan(field_idx(m))
+            % Trials with activation at peak field
+            activeTrials = find(sum([rmaps1(:, delay(field_idx(m)), cellIdx(m)); ...
+                                     rmaps2(:, delay(field_idx(m)), cellIdx(m))], 2) > 0);
+            activation_probability(m) = length(activeTrials) / size(rmap, 1);
+
+            % Variance of peak time across trials (converted to seconds)
+            [~, peak_locs] = max(rmap, [], 2);
+            peak_time_variance(m) = nanvar(peak_locs * 0.1613);  % time bin duration
         end
-        
     end
-
 end
