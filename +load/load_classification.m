15d190fc4a0e7911a9828d5e485b35995d996fc1
annacga <166383866+annacga@users.noreply.github.com>
2025-05-16 15:44:53 +0200

    Update load_classification.m

--- +load/load_classification.m
+++ +load/load_classification.m
@@ -6,7 +6,7 @@ else
     load_data = 1:length(data)-1;
 end
 
-loaddir = '/Users/annachristinagarvert/UIO Physiology Dropbox Dropbox/Lab Data/Malte Bieler/Fig_1/classification_';
+loaddir = '/Fig_1/classification';
 for i =load_data
     for f = 1:length(data(i).sessionIDs)
                           
